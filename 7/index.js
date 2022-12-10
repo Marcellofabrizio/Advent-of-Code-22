dd06f4dd15bb51a5e3b15d4691dcef87f73f3d5d
Marcello Fabrizio <marcello@pop-os.localdomain>
2022-12-10 00:44:30 -0300

    Day 7

--- 7/index.js
+++ 7/index.js
@@ -1,45 +1,46 @@
 const fs = require("fs");
 const input = fs.readFileSync("input.txt", "utf8").split("\n");
 
-dirSum = 0;
-dirs = [];
-fileSizes = {};
+currentDirsum = 0;
+currentDirs = [];
+fileSizes = { "/": 0 };
 
 input.forEach((line) => {
   const command = line.split(" ");
-  switch (command.length) {
-    case 2:
-      if (command[0] == "dir") {
-        break;
-      } else if (command[0] == "$") {
-        break;
-      } else {
-        dirs.reduce((str, i) => {
-          str = str + i;
-          console.log(str);
-          if (fileSizes[str]) fileSizes[str] += parseInt(command[0]);
-          else fileSizes[str] = parseInt(command[0]);
-        });
-      }
+  switch (command[0]) {
+    case "dir":
       break;
-    case 3:
-      if (command[1] == "cd") {
+    case "$":
+      if (command[1] == "ls") {
+        break;
+      } else if (command[1] == "cd") {
         if (command[2] == "..") {
-          dirs.pop();
+          currentDirs.pop();
         } else if (command[2] == "/") {
-          dirs = ["/"];
+          currentDirs = ["/"];
         } else {
-          dirs.push(command[2] + "/");
+          currentDirs.push(command[2] + "/");
         }
       }
       break;
+    default:
+      auxStr = "";
+      fileSizes["/"] += parseInt(command[0]);
+      currentDirs.reduce((str, i) => {
+        if (fileSizes[str + i]) fileSizes[str + i] += parseInt(command[0]);
+        else fileSizes[str + i] = parseInt(command[0]);
+        return str + i;
+      });
+      break;
   }
 });
-
-console.log(dirs.reduce((str, i) => str + i));
 
 console.log(
   Object.values(fileSizes)
     .filter((i) => i <= 100000)
     .reduce((acc, i) => acc + i, 0)
+);
+
+console.log(
+  Math.min(...Object.values(fileSizes).filter((i) => i >= fileSizes["/"] - 40000000))
 );
