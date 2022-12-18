099f9b7f75f75dc97094dd4e65150fe4d3a018e9
Marcellofabrizio <marcfabrizio10@gmail.com>
2022-12-18 18:29:35 +0100

    Class rework and part 2

--- 11/main.py
+++ 11/main.py
@@ -1,99 +1,91 @@
 import math
-INPUT = "input.txt"
+from functools import reduce
+
+file = open("input.txt", 'r')
+input = [line.split() for line in file if line.strip() != '']
 
 
-class Monkey():
-    def __init__(self, index, items_input, operation_input, test_input, condition_input_true, condition_input_false):
-        self.id = index
-        self.items = parse_items_input(items_input)
-        self.operation, self.worry_lvl = parse_operation_input(operation_input)
-        self.test = parse_test_input(test_input)
-        self.condition_true = parse_condition_input(condition_input_true)
-        self.condition_false = parse_condition_input(condition_input_false)
+class Monkey:
+
+    def __init__(self, id=0, items=None, opperation=None, test=None, action=[]):
+        self.id = id
+        self.items = items
+        self.opperation = opperation
+        self.test = test
+        self.action = action
         self.monki_bsns = 0
 
-
-def parse_items_input(input):
-    items = input.split(": ")[1].split(", ")
-    return items
+    def __str__(self):
+        return f"{self.id} , {self.items} , {self.opperation} , {self.test} , {self.action}"
 
 
-def parse_operation_input(input):
-    operation = input.split("= ")[1].split(" ")[1]
-    worry_lvl = input.split("= ")[1].split(" ")[2]
-    operation_func = None
+def parse_input():
+    monkeys = []
+    for i in range(0, len(input) - 1, 6):
+        for j in input[i:i + 6]:
+            monkey = Monkey(id=int(input[i:i + 6][0][1].strip(":")))
 
-    if operation == "*":
-        def operation_func(x, y): return x*y
-    elif operation == "+":
-        def operation_func(x, y): return x+y
-    elif operation == "-":
-        def operation_func(x, y): return x-y
+            string = "".join(input[i:i + 6][1][2:])
+            monkey.items = [int(s) for s in string.split(',')]
 
-    return (operation_func, worry_lvl)
+            monkey.opperation = (input[i:i + 6][2][4], int(input[i:i + 6][2][5])
+                                 if input[i:i + 6][2][5] != "old" else "*2")
 
+            monkey.test = int(input[i:i + 6][3][3])
 
-def parse_test_input(input):
-    return int(input.split("by ")[1])
+            monkey.condition_true = int(input[i:i + 6][4][5])
+            monkey.condition_false = int(input[i:i + 6][5][5])
 
+            monkeys.append(monkey)
 
-def parse_condition_input(input):
-    return int(input.split("monkey ")[1])
-
-
-def read_input():
-    input = [line.strip() for line in open(INPUT)]
-    return input
+            break
+    return monkeys
 
 
-input = read_input()
-print(input, int(len(input)/6))
-monkey_index = 0
+def operation(lvl, t_ops):
+    if isinstance(t_ops[1], int):
+        if t_ops[0] == '*':
+            lvl *= t_ops[1]
+        else:
+            lvl += t_ops[1]
+    else:
+        lvl *= lvl
 
-monkeys = [Monkey(0, input[1], input[2], input[3], input[4], input[5])]
-monkey_index += 1
-for i in range(5, len(input)):
-    if input[i] == "":
-        i += 1
-        monkeys.append(
-            Monkey(monkey_index, input[i+1], input[i+2], input[i+3], input[i+4], input[i+5]))
-        monkey_index += 1
+    return lvl
 
-for _ in range(0, 20):
+
+monkeys = parse_input()
+lcm = 1
+for monkey in monkeys:
+    lcm = (lcm*monkey.test)
+print(lcm)
+
+for r in range(10000):
     for monkey in monkeys:
-        print(f"Monkey {monkey.id}")
-        old_list = monkey.items.copy()
+        monkey.monki_bsns += len(monkey.items)
         for i, item in enumerate(monkey.items):
-            print(f"  Monkey inspects an item with a worry level of {item}")
-            monkey.monki_bsns += 1
-            if monkey.worry_lvl == "old":
-                new_lvl = monkey.operation(int(item), int(item))
-            else:
-                new_lvl = monkey.operation(int(item), int(monkey.worry_lvl))
-            print(f"    Worry level is multiplied by {monkey.worry_lvl} to {new_lvl}")
-    
-            division = math.floor(new_lvl / 3)
-            print(f"    Monkey gets bored with item. Worry level is divided by 3 to {division}")
-    
-            if division % monkey.test == 0:
-                print(f"    Current worry level is divisible by {monkey.test}")
+            new_lvl = operation(item, monkey.opperation)
+
+            new_lvl %= lcm
+
+            if new_lvl % monkey.test == 0:
                 new_monkey = monkey.condition_true
-                print(f"    Item with worry level {division} is throw to monkey {new_monkey}")
-                monkeys[new_monkey].items.append(division)
-                old_list.remove(item)
+                monkeys[new_monkey].items.append(new_lvl)
             else:
-                print(f"    Current worry level is not divisible by {monkey.test}")
                 new_monkey = monkey.condition_false
-                print(f"    Item with worry level {division} is throw to monkey {new_monkey}")
-                monkeys[new_monkey].items.append(division)
-                old_list.remove(item)
-    
-        monkey.items = old_list
-    
+                monkeys[new_monkey].items.append(new_lvl)
+
+        monkey.items.clear()
+
+    if r in [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
+        print(f"== After round {r} ==")
+        for monkey in monkeys:
+            print(f"Monkey {monkey.id}", monkey.monki_bsns)
+
 for monkey in monkeys:
     print(f"Monkey {monkey.id}", monkey.items)
 
-monkeys.sort(key = lambda x : x.monki_bsns, reverse=True)
+monkeys.sort(key=lambda x: x.monki_bsns, reverse=True)
 for monkey in monkeys:
     print(f"Monkey {monkey.id}", monkey.monki_bsns)
 
